6481d98f21e849e74d9b6f3186b87c466b9091f5
JoseA46 <jialfonsoc@academia.usbbog.edu.co>
2023-03-09 20:50:24 -0500

    mensaje
    se inserta el portal del mensaje

--- correodevoz.php
+++ correodevoz.php
@@ -27,8 +27,17 @@ try
     $mail->setFrom('jialfonsoc@academia.usbog.edu.co');
     $mail->addAddress('joseealfonss12@gmail.com');
 
+    $mail->isHTML(true);
+    $mail->subject = 'contacto desde formulario';
+    $mail->Body = 'Este es el espacio del mensaje <b>en negrita</b>';
+    $mail->Altbody = 'Este es el contenido del mensaje en texto';
 
+    //envia el email
+    $mail->sent();
+
+    echo 'El mensaje ha sido enviado';
+} 
+catch (Exception $e) {
+    echo 'El mensaje no ha sido enviado, error: ', $mail->ErrorInfo;
 }
-catch
-
 ?>
