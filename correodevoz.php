63022caea7a2159a2fdb4c67aef8b853c6f427cd
JoseA46 <jialfonsoc@academia.usbbog.edu.co>
2023-03-09 20:25:54 -0500

    correodevoz
    libreria del correo de voz con las librerias

--- correodevoz.php
+++ correodevoz.php
@@ -1,5 +1,32 @@
 <?php
 
+use PHPMaiier\PHPMailer\PHPMailer;
+use PHPMaiier\PHPMailer\PException;
 
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMIP.php';
+
+$mail = new PHPMailer(true);
+
+
+try
+{
+    $mail->SMTPDebug =2;
+    $mail->isSMP();
+    $mail->Host = 'smtp.mailtrap.io';
+    $mail->SMTPAuth = true;
+    $mail->Username = '4665eb0a94c6ff':
+    $mail->Password = '9154f428acd8c7';
+    $mail->SMTPSecure ='tls';
+    $mail->port = 587;
+
+
+    $mail->setFrom('jialfonsoc@academia.usbog.edu.co');
+    $mail->addAddress('joseealfonss12@gmail.com');
+    
+
+}
+catch
 
 ?>
